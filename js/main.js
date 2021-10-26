8d7110be1e5991390630a21cfdba0cbe6e27811b
“Cal-107” <kaly___7@hotmail.it>
2021-10-26 15:45:48 +0200

    add fix

--- js/main.js
+++ js/main.js
@@ -8,14 +8,12 @@
 //creazione array email
 
 const mailList = ['blu@gmail.it', 'rosso@gmail.it', 'verde@gmail.it', 'viola@gmail.it', 'marrone@gmail.it'];
-console.log(mailList);
 
 //numero tot di item array
 console.log(mailList.length);
 
 //chiedere email all'utente
 let mailChoise = prompt('Inserisca un e-mail valida presente in lista');
-console.log(mailChoise);
 
 //check se l'email inserita è presente nella lista array
 
@@ -30,18 +28,8 @@ console.log(mailChoise);
 
     if (finalChoise) {
         alert('E-mail valida, grazie');
-        console.log('Email valida');
     } else {
         alert('E-mail non valida, inserire e-mail presente in lista');
-        console.log('email non valida');
     }
-   
-
-    // for (let i= 0; i < mailList.length; i++) {
-    //     if (mailList[i] !== finalChoise) {
-    //         finalChoise = true;
-    //     }
-    //     console.log(`L'E-mail scelta ${finalChoise} non è disponibile nella lista`)
-    // }
 
 
