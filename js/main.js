4e249594587f5e2567572daa4e56d1fe68808a54
“Cal-107” <kaly___7@hotmail.it>
2021-10-26 13:34:43 +0200

    add setup

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Mail
+// - crea una lista contenente almeno 3 indirizzi email
+// - chiedi all’utente la sua email,
+// - controlla che sia nella lista di chi può acceder (lista creata sopra al punto 1),
+// - stampa un messaggio appropriato sull’esito del controllo.
+
+
+//creazione array email
+
+const mailList = ['blu@gmail.it', 'rosso@gmail.it', 'verde@gmail.it']
+console.log(mailList)
+
+//numero tot di item array
+console.log(mailList.lenght)
+
+//chiedere email all'utente
+let mailChoise = prompt('Inserisca la sua email')
+console.log(mailChoise)
+
